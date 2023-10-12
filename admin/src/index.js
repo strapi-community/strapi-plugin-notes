3ecd58c911e5cd31873a96adcb7ef232bcaa8442
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-10-12 00:29:58 -0400

    feat(admin): add translation support

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,6 +2,7 @@ import pluginPkg from '../../package.json';
 import { pluginId } from './pluginId';
 import Initializer from './components/Initializer';
 import NoteListLayout from './components/NoteListLayout';
+import { prefixPluginTranslations } from '@strapi/helper-plugin';
 
 const name = pluginPkg.strapi.name;
 
@@ -20,5 +21,29 @@ export default {
 			name: 'note-list',
 			Component: NoteListLayout,
 		});
+	},
+
+	async registerTrads({ locales }) {
+		const importedTrads = await Promise.all(
+			locales.map((locale) => {
+				return import(
+					/* webpackChunkName: "translation-[request]" */ `./translations/${locale}.json`
+				)
+					.then(({ default: data }) => {
+						return {
+							data: prefixPluginTranslations(data, pluginId),
+							locale,
+						};
+					})
+					.catch(() => {
+						return {
+							data: {},
+							locale,
+						};
+					});
+			})
+		);
+
+		return Promise.resolve(importedTrads);
 	},
 };
